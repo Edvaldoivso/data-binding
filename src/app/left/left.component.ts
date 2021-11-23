416f6e4fbaa94197bdabad015d63a01ed351d147
Edvaldo I Souza <edvaldoivosouza@ymail.com>
2021-11-23 01:02:53 -0300

    Adicionei um teste de Whats

--- src/app/left/left.component.ts
+++ src/app/left/left.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftComponent implements OnInit {
 
+
+
+
+//URL DE Previsão de Tempo
+//https://api.weatherapi.com/v1/current.json?key=779744a5afc94dd4aff05614212311&q=S%C3%A3o%20Paulo
+
+
+
   constructor() { }
 
   ngOnInit(): void {
